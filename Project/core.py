1b9d83efc2644b5bdd3efc559d737b283abbc6db
kylekap <kylekaptbone@yahoo.com>
2021-11-07 18:22:57 -0500

    Setting up project

---
+++ Project/core.py
@@ -0,0 +1,14 @@
+#https://realpython.com/python-pep8/
+
+def modular_function():
+    
+    return None
+
+
+def main():
+    # main function that you use to run the program
+    modular_function()
+
+
+if __name__ == '__main__':
+    main()
